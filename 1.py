969b44537a4b97998f4a9d60925718b79c078f8d
Zhorik <LeoGeorg25150901@yandex.ru>
2023-04-07 21:47:31 +0300

    Заготовка_2

--- 1.py
+++ 1.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask, render_template, redirect, make_response, jsonify
-from data import db_session
+from flask import Flask, render_template, redirect, request, make_response, session, abort, jsonify
+from data import db_session, news_api
 from data.users import User
+from data.news import News
+import datetime
 from forms.user import RegisterForm, LoginForm
-from flask_login import LoginManager, login_user, logout_user, login_required
-from flask_restful import Api
+from forms.news import NewsForm
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_restful import reqparse, abort, Api, Resource
+from data import news_resources
+
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -24,9 +29,39 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route('/')
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60)
+    return res
+
+
+@app.route("/")
 def index():
-    return render_template('index.html', title='Погода')
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/logout')
@@ -34,7 +69,6 @@ def index():
 def logout():
     logout_user()
     return redirect("/")
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -76,5 +110,81 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
+# @app.route('/news',  methods=['GET', 'POST'])
+# @login_required
+# def add_news():
+#     form = NewsForm()
+#     if form.validate_on_submit():
+#         db_sess = db_session.create_session()
+#         news = News()
+#         news.title = form.title.data
+#         news.content = form.content.data
+#         news.is_private = form.is_private.data
+#         current_user.news.append(news)
+#         db_sess.merge(current_user)
+#         db_sess.commit()
+#         return redirect('/')
+#     return render_template('news.html', title='Добавление новости',
+#                            form=form)
+#
+#
+# @app.route('/news&<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def edit_news(id):
+#     form = NewsForm()
+#     if request.method == "GET":
+#         db_sess = db_session.create_session()
+#         news = db_sess.query(News).filter(News.id == id,
+#                                           News.user == current_user
+#                                           ).first()
+#         if news:
+#             form.title.data = news.title
+#             form.content.data = news.content
+#             form.is_private.data = news.is_private
+#         else:
+#             abort(404)
+#     if form.validate_on_submit():
+#         db_sess = db_session.create_session()
+#         news = db_sess.query(News).filter(News.id == id,
+#                                           News.user == current_user
+#                                           ).first()
+#         if news:
+#             news.title = form.title.data
+#             news.content = form.content.data
+#             news.is_private = form.is_private.data
+#             db_sess.commit()
+#             return redirect('/')
+#         else:
+#             abort(404)
+#     return render_template('news.html',
+#                            title='Редактирование новости',
+#                            form=form
+#                            )
+#
+#
+# @app.route('/news_delete&<int:id>', methods=['GET', 'POST'])
+# @login_required
+# def news_delete(id):
+#     db_sess = db_session.create_session()
+#     news = db_sess.query(News).filter(News.id == id,
+#                                       News.user == current_user
+#                                       ).first()
+#     if news:
+#         db_sess.delete(news)
+#         db_sess.commit()
+#     else:
+#         abort(404)
+#     return redirect('/')
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    # app.register_blueprint(news_api.blueprint)
+    # api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+    # api.add_resource(news_resources.NewsResource, '/api/v2/news&<int:news_id>')
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    main()

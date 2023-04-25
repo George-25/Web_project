6a1427c129df346d813038126d2047dd78018cc6
Zhorik <LeoGeorg25150901@yandex.ru>
2023-04-26 01:20:41 +0300

    weatherapi

--- 1.py
+++ 1.py
@@ -12,8 +12,9 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-appid = "078007e7d84335abcc1c77d2160b20e3"
+openweathermap_key = "078007e7d84335abcc1c77d2160b20e3"
 accuweather_key = "xVwx5iGlsnoziBNp1ZbGJJnJ1PfO1tPh"
+weatherapi_key = '883b49c44e0542b6b51201949232504'
 api = Api(app)
 
 
@@ -75,17 +76,17 @@ def foo():
     # conn.close()
     try:
         res = requests.get("http://api.openweathermap.org/data/2.5/weather",
-                           params={'q': name_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+                           params={'q': name_city, 'units': 'metric', 'lang': 'ru', 'APPID': openweathermap_key})
         data = res.json()
-        par_0 = 'Openweathermap.org'
-        par_1 = f"{upcase_first_letter(data['weather'][0]['description'])}"
-        par_2 = f"Температура: {data['main']['temp']} ℃"
-        par_3 = f"Ощущается как: {data['main']['feels_like']} ℃"
-        par_4 = f"Минимальная температура: {data['main']['temp_min']} ℃"
-        par_5 = f"Максимальная температура: {data['main']['temp_max']} ℃"
-        par_6 = f"Атмосферное давление: {data['main']['pressure']} hPa"
-        par_7 = f"Влажность воздуха: {data['main']['humidity']} %"
-        # --------------------------------------------------------------------------------------
+        openweather_par0 = 'Openweathermap.org'
+        openweather_par1 = f"{upcase_first_letter(data['weather'][0]['description'])}"
+        openweather_par2 = f"Температура: {data['main']['temp']} ℃"
+        openweather_par3 = f"Ощущается как: {data['main']['feels_like']} ℃"
+        openweather_par4 = f"Минимальная температура: {data['main']['temp_min']} ℃"
+        openweather_par5 = f"Максимальная температура: {data['main']['temp_max']} ℃"
+        openweather_par6 = f"Атмосферное давление: {data['main']['pressure']} hPa"
+        openweather_par7 = f"Влажность воздуха: {data['main']['humidity']} %"
+        # --------------------------------------------------------------------------------------------------------------
         res_location = requests.get("http://dataservice.accuweather.com/locations/v1/cities/search?",
                                     params={'apikey': accuweather_key, 'q': name_city2, 'language': 'ru'})
         data_2 = res_location.json()
@@ -94,31 +95,41 @@ def foo():
                                 params={'apikey': accuweather_key, 'metric': 'true',
                                         'language': 'ru'})
         data_3 = res_temp.json()
-        par0 = 'Accuweather.com'
-        par1 = 'Сводка за полседний час:'
-        par2 = f"{upcase_first_letter(data_3[0]['IconPhrase'])}"
-        par3 = f"Температура: {data_3[0]['Temperature']['Value']} ℃"
-        par4 = f"Вероятность осадков: {data_3[0]['PrecipitationProbability']} %"
-    except Exception as e:
+        accuweather_par0 = 'Accuweather.com'
+        accuweather_par1 = 'Сводка за полседний час:'
+        accuweather_par2 = f"{upcase_first_letter(data_3[0]['IconPhrase'])}"
+        accuweather_par3 = f"Температура: {data_3[0]['Temperature']['Value']} ℃"
+        accuweather_par4 = f"Вероятность осадков: {data_3[0]['PrecipitationProbability']} %"
+        # --------------------------------------------------------------------------------------------------------------
+        res_w = requests.get("http://api.weatherapi.com/v1/current.json?",
+                             params={'key': weatherapi_key, 'lang': 'ru', 'query': name_city2})
+        data_4 = res_w.json()
+        weatherapi_par0 = 'Weatherapi.com'
+        weatherapi_par1 = f"{upcase_first_letter(data_4['current']['condition']['text'])}"
+        weatherapi_par2 = f"Температура: {data_4['current']['temp_c']} ℃"
+        weatherapi_par3 = f"Ощущается как: {data_4['current']['feelslike_c']} ℃"
+        weatherapi_par4 = f"Скорость ветра: {data_4['current']['wind_kph']} км/ч"
+        weatherapi_par5 = f"Атмосферное давление: {str(int(data_4['current']['pressure_mb']))} hPa"
+        weatherapi_par6 = f"Количество осадков: {data_4['current']['precip_mm']} мм"
+        weatherapi_par7 = f"Влажность воздуха: {data_4['current']['humidity']} %"
+        weatherapi_par8 = f"Облачный покров: {data_4['current']['cloud']} %"
+    except Exception:
         city_param = 'Город с таким названием не найден.'
-        par_0 = ''
-        par_1 = ''
-        par_2 = ''
-        par_3 = ''
-        par_4 = ''
-        par_5 = ''
-        par_6 = ''
-        par_7 = ''
-        par0 = ''
-        par1 = ''
-        par2 = ''
-        par3 = ''
-        par4 = ''
+        return render_template("index.html", city_param=city_param)
     return render_template("get_text.html",
                            city_param=city_param,
-                           par_0=par_0, par_1=par_1, par_2=par_2, par_3=par_3, par_4=par_4, par_5=par_5, par_6=par_6,
-                           par0=par0, par1=par1, par2=par2, par3=par3, par4=par4,
-                           par_7=par_7
+                           openweather_par0=openweather_par0, openweather_par1=openweather_par1,
+                           openweather_par2=openweather_par2, openweather_par3=openweather_par3,
+                           openweather_par4=openweather_par4, openweather_par5=openweather_par5,
+                           openweather_par6=openweather_par6, openweather_par7=openweather_par7,
+                           accuweather_par0=accuweather_par0, accuweather_par1=accuweather_par1,
+                           accuweather_par2=accuweather_par2, accuweather_par3=accuweather_par3,
+                           accuweather_par4=accuweather_par4,
+                           weatherapi_par0=weatherapi_par0, weatherapi_par1=weatherapi_par1,
+                           weatherapi_par2=weatherapi_par2, weatherapi_par3=weatherapi_par3,
+                           weatherapi_par4=weatherapi_par4, weatherapi_par5=weatherapi_par5,
+                           weatherapi_par6=weatherapi_par6, weatherapi_par7=weatherapi_par7,
+                           weatherapi_par8=weatherapi_par8
                            )
 
 
